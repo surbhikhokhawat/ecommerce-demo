5939827c4812b916fef9574bd00e79d579374a29
Surbhi Khokhawat <surbhikhokhawat@shubhams-MacBook-Air.local>
2021-02-09 17:23:12 +0530

    resolved total price problem in cart

--- store.js
+++ store.js
@@ -3,8 +3,11 @@
     {
         var itemName=e.parentNode.querySelector(".item-name").innerHTML;
         var itemPrice = e.parentNode.querySelector(".item-price").innerHTML;
-        totalPrice = totalPrice + parseInt(itemPrice);
-        $("#cart").append("<div class='product'><img src='"+e.parentNode.parentNode.querySelector(".item-img").src+"'><div class='product-info'><h2 class='product-name'>"+itemName+"</h2><h4 class='product-price'></h4><h5 class='product-offer'></h5><p class='product-quantity'>Qnty: <input value='1' name=''></p><p class='product-remove'><i class='fa fa-trash' aria-hidden='true'></i><span class='remove'>Remove</span></p></div></div>"
+        var itemQty = e.parentNode.querySelector(".qty").value;
+        console.log(itemQty);
+        totalPrice =totalPrice + itemPrice * itemQty;
+        console.log(totalPrice);
+        $("#cart").append("<div class='product'><img src='"+e.parentNode.parentNode.querySelector(".item-img").src+"'><div class='product-info'><h2 class='product-name'>"+itemName+"</h2><h4 class='product-price'>"+itemPrice+"</h4><h5 class='product-offer'></h5><p class='product-quantity'>Qnty: <input value='"+itemQty+"' name=''></p><p class='product-remove'><i class='fa fa-trash' aria-hidden='true'></i><span class='remove'>Remove</span></p></div></div>"
         )
         document.getElementById("total-price").innerHTML = totalPrice;
     }
